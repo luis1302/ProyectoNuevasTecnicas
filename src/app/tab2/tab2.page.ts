ea4f9a3a6c5c484cd69c1f8e7ada52c016c49e31
luis1302 <luis-adrian1302@hotmail.com>
2019-03-16 23:36:51 -0500

    version2

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -5,4 +5,12 @@ import { Component } from '@angular/core';
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+export class Tab2Page {
+constructor(private navCtrl: NavController){
+}
+
+routingViaNavParams(event){
+  this.navCtrl.navigateForward('/tabs/tab3');
+}
+
+}
